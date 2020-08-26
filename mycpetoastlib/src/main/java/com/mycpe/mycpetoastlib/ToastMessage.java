1b5128034a4a7b9639da567a145c60e92f1a0b27
kishansh31 <55207490+kishansh31@users.noreply.github.com>
2020-08-26 20:13:40 +0530

    initial commit with the toastLib

---
+++ mycpetoastlib/src/main/java/com/mycpe/mycpetoastlib/ToastMessage.java
@@ -0,0 +1,14 @@
+package com.mycpe.mycpetoastlib;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastMessage {
+
+    public static void s(Context context, String message) {
+
+        Toast.makeText(context, ""+message, Toast.LENGTH_SHORT).show();
+
+    }
+
+}
